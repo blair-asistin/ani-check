f53554f0b3a4c44bd4039fdf2fe02daa55844cc1
FlowerKnight55 <162504966+FlowerKnight55@users.noreply.github.com>
2025-01-17 20:59:26 -0500

    Added test API for Anilist

--- src/app/dashboard/dashboard.component.ts
+++ src/app/dashboard/dashboard.component.ts
@@ -1,4 +1,19 @@
+import { Apollo, gql } from 'apollo-angular';
 import { Component } from '@angular/core';
+
+const GETPOST = gql`
+  query {
+    Page {
+      media(search: "Naruto", type: ANIME) {
+        id
+        title {
+          romaji
+          english
+          native
+        }
+      }
+    }
+  }`;
 
 @Component({
   selector: 'app-dashboard',
@@ -7,5 +22,22 @@ import { Component } from '@angular/core';
   styleUrl: './dashboard.component.css'
 })
 export class DashboardComponent {
+  posts: any[] = [];
+  loading = true;
+  error: any;
 
+  constructor(private apollo: Apollo){}
+
+  ngOnInit() {
+    this.apollo
+    .watchQuery({
+      query: GETPOST,
+    })
+    .valueChanges.subscribe(({ data, loading, error }: any) => {
+      this.loading = loading;
+      this.error = error;
+      this.posts = data.Page.media;
+    });
+    
+  }
 }
